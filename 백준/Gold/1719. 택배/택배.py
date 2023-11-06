20b08811386b4c405b238f9b67dbb7c661856842
Mireu Kim <67996276+kimmireu0220@users.noreply.github.com>
2023-11-06 23:32:34 +0900

    [Gold III] Title: 택배, Time: 284 ms, Memory: 112844 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,26 +1,19 @@
-import sys
-INF = int(1e8)
-def input(): return sys.stdin.readline().rstrip()
-
-n,m = map(int, input().split())
-graph =[[INF] * (n+1) for _ in range(n+1)]
-node = [[0] * (n+1) for _ in range(n+1)]
+INF = int(1e9)
+n, m = map(int, input().split())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+paths = [[0] * (n + 1) for _ in range(n + 1)]
 for _ in range(m):
-    a, b, c, = map(int, input().split())
-    graph[a][b] = c
-    graph[b][a] = c
-    node[a][b] = b
-    node[b][a] = a
-
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
-            if graph[a][b] > graph[a][k] + graph[k][b]:
-                graph[a][b] = graph[a][k] + graph[k][b]
-                node[a][b] = node[a][k]
-
-for i in range(1,n+1):
-    node[i][i] = '-'
-
-for array in node[1:]:
-    print(*array[1:])
+    a, b, cost = map(int, input().split())
+    graph[a][b], graph[b][a] = cost, cost
+    paths[a][b], paths[b][a] = b, a
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            temp = graph[a][k] + graph[k][b]
+            if graph[a][b] > temp:
+                graph[a][b] = temp
+                paths[a][b] = paths[a][k]
+for i in range(1, n + 1):
+    paths[i][i] = "-"
+for i in paths[1:]:
+    print(*i[1:])
