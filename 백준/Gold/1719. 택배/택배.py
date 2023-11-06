84546fb5767a76fa875d8a6a5b1ae2fb19721fd1
Mireu Kim <67996276+kimmireu0220@users.noreply.github.com>
2023-11-06 23:29:42 +0900

    [Gold III] Title: 택배, Time: 296 ms, Memory: 112720 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,26 @@
+import sys
+INF = int(1e8)
+def input(): return sys.stdin.readline().rstrip()
+
+n,m = map(int, input().split())
+graph =[[INF] * (n+1) for _ in range(n+1)]
+node = [[0] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b, c, = map(int, input().split())
+    graph[a][b] = c
+    graph[b][a] = c
+    node[a][b] = b
+    node[b][a] = a
+
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            if graph[a][b] > graph[a][k] + graph[k][b]:
+                graph[a][b] = graph[a][k] + graph[k][b]
+                node[a][b] = node[a][k]
+
+for i in range(1,n+1):
+    node[i][i] = '-'
+
+for array in node[1:]:
+    print(*array[1:])
